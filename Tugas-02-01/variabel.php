f6b67a19fd6b2436b6ebb41fd57125dbcb15aae1
Ridha30 <92918991+Ridha30@users.noreply.github.com>
2021-11-02 20:10:04 -0700

    Add files via upload

---
+++ Tugas-02-01/variabel.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Ini adalah nilai awal variabel
+*/
+$nama = "Ridha Nur Ilahi";
+$usia = 20;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
+berusia ".$usia." tahun<p>";
+
+?>
